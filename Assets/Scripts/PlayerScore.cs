d384873307672f5b10a9880a0199985178ea9697
ninetynineknights <morgan@morgans-mbp.lan>
2020-01-16 19:43:00 -0600

    update some buggy going on

---
+++ Assets/Scripts/PlayerScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScore : MonoBehaviour
+{
+    public float timeLeft = 120;
+    public int playerScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        //Debug.Log(timeLeft); //to check the time running down
+
+        if(timeLeft < 1)
+        {
+            SceneManager.LoadScene("Prototype_1");
+        }
+    }
+}
